2d6604de6172b02a690dde8ea3553acfef1c9aa8
castellano <>
2006-09-30 21:00:26 +0000

    more progname stuff

--- paclink-unix/progname.h
+++ paclink-unix/progname.h
@@ -5,16 +5,23 @@
 #ifndef PROGNAME_H
 #define PROGNAME_H
 
-#if HAVE___ARGV
-extern char **__Argv;
-#define progname __Argv[0]
+#if HAVE_GETPROGNAME
+#define progname getprogname()
 #else
 #if HAVE___PROGNAME
 extern char *__progname;
 #define progname __progname
 #else
+#if HAVE___ARGV
+extern char **__Argv;
+#define progname __Argv[0]
+#else
+#if HAVE____ARGV
+extern char **___Argv;
+#define progname ___Argv[0]
+#else
 #define progname "(unknown program)"
 #endif
 #endif
-
+#endif
 #endif
