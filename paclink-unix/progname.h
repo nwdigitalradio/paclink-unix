f6c30af992b479dc41cd14e45ea47ccd52523725
castellano <>
2006-09-30 20:36:48 +0000

    autoconfify the progname stuff

--- paclink-unix/progname.h
+++ paclink-unix/progname.h
@@ -5,13 +5,15 @@
 #ifndef PROGNAME_H
 #define PROGNAME_H
 
-#if defined(__osf__) || defined(__solaris__)
+#if HAVE___ARGV
 extern char **__Argv;
 #define progname __Argv[0]
 #else
-#if defined(__NetBSD__) || defined(__linux__)
+#if HAVE___PROGNAME
 extern char *__progname;
 #define progname __progname
+#else
+#define progname "(unknown program)"
 #endif
 #endif
 
