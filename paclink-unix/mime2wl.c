26cbe81f9b09af6cbe05d33b39ea00b03297b3d1
castellano <>
2006-10-05 17:48:29 +0000

    Take some baby steps toward using gmime library for MIME parsing.

--- paclink-unix/mime2wl.c
+++ paclink-unix/mime2wl.c
@@ -1,3 +1,27 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 2; c-brace-offset: -2; c-argdecl-indent: 2 -*- */
+/*
+ *  Authors: Jeffrey Stedfast <fejj@helixcode.com>
+ *           Nicholas S. Castellano N2QZ <n2qz@arrl.net>
+ *
+ *  Copyright 2000 Helix Code, Inc. (www.helixcode.com)
+ *  Copyright 2006 Nicholas S. Castellano <n2qz@arrl.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *
+ */
+
 #if HAVE_CONFIG_H
 # include "config.h"
 #endif
@@ -7,11 +31,25 @@
 __RCSID("$Id$");
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <time.h>
+#if HAVE_STDIO_H
+# include <stdio.h>
+#endif
+#if HAVE_STDLIB_H
+# include <stdlib.h>
+#endif
+#if HAVE_STRING_H
+# include <string.h>
+#endif
+#if HAVE_CTYPE_H
+# include <ctype.h>
+#endif
+#if HAVE_TIME_H
+# include <time.h>
+#endif
+#if HAVE_FCNTL_H
+# include <fcntl.h>
+#endif
+#include <gmime/gmime.h>
 #include "llist.h"
 #include "buffer.h"
 #include "strutil.h"
@@ -331,17 +369,114 @@ mime2wl(struct buffer *mime, const char *callsign)
   /* XXX finish me */
 }
 
+static void
+write_gmimeobject_to_screen(GMimeObject *object)
+{
+  GMimeStream *stream;
+	
+  /* create a new stream for writing to stdout */
+  stream = g_mime_stream_fs_new(dup(1));
+	
+  /* write the object to the stream */
+  g_mime_object_write_to_stream(object, stream);
+	
+  /* flush the stream (kinda like fflush() in libc's stdio) */
+  g_mime_stream_flush(stream);
+	
+  /* free the output stream */
+  g_object_unref (stream);
+}
+
+static GMimeMessage *
+parse_message(int fd)
+{
+  GMimeMessage *message;
+  GMimeParser *parser;
+  GMimeStream *stream;
+	
+  /* create a stream to read from the file descriptor */
+  stream = g_mime_stream_fs_new(dup(fd));
+	
+  /* create a new parser object to parse the stream */
+  parser = g_mime_parser_new_with_stream(stream);
+	
+  /* unref the stream (parser owns a ref, so this object does not actually get free'd until we destroy the parser) */
+  g_object_unref(stream);
+	
+  /* parse the message from the stream */
+  message = g_mime_parser_construct_message(parser);
+	
+  /* free the parser (and the stream) */
+  g_object_unref(parser);
+	
+  return message;
+}
+
+static void
+count_foreach_callback(GMimeObject *part, gpointer user_data)
+{
+  GMimeContentType *content_type;
+  int *count = user_data;
+	
+  (*count)++;
+	
+  /* 'part' points to the current part node that g_mime_message_foreach_part() is iterating over */
+	
+  /* find out what class 'part' is... */
+  if (GMIME_IS_MESSAGE_PART(part)) {
+    /* message/rfc822 or message/news */
+    GMimeMessage *message;
+		
+    /* g_mime_message_foreach_part() won't descend into
+       child message parts, so if we want to count any
+       subparts of this child message, we'll have to call
+       g_mime_message_foreach_part() again here. */
+		
+    message = g_mime_message_part_get_message((GMimeMessagePart *) part);
+    g_mime_message_foreach_part(message, count_foreach_callback, count);
+    g_object_unref(message);
+  } else if (GMIME_IS_MESSAGE_PARTIAL(part)) {
+    /* message/partial */
+		
+    /* this is an incomplete message part, probably a
+       large message that the sender has broken into
+       smaller parts and is sending us bit by bit. we
+       could save some info about it so that we could
+       piece this back together again once we get all the
+       parts? */
+  } else if (GMIME_IS_MULTIPART(part)) {
+    /* multipart/mixed, multipart/alternative, multipart/related, multipart/signed, multipart/encrypted, etc... */
+		
+    /* we'll get to finding out if this is a signed/encrypted multipart later... */
+  } else if (GMIME_IS_PART(part)) {
+    /* a normal leaf part, could be text/plain or image/jpeg etc */
+    printf("leaf part\n");
+    write_gmimeobject_to_screen(part);
+  } else {
+    g_assert_not_reached();
+  }
+}
+
 int
 main()
 {
   struct buffer *mime;
   struct buffer *wl;
+  int fd;
+  GMimeMessage *message;
+  int count = 0;
 
-  if ((mime = buffer_readfile("mime.msg")) == NULL) {
-    perror("buffer_readfile");
+  g_mime_init(0);
+
+  if ((fd = open("mime.msg", O_RDONLY)) == -1) {
+    perror("open");
     exit(2);
   }
-  wl = mime2wl(mime, "N2QZ");
+  message = parse_message(fd);
+  close(fd);
+  g_mime_message_foreach_part(message, count_foreach_callback, &count);
+  printf("There are %d parts in the message\n", count);
+  g_mime_shutdown();
   exit(0);
   return 1;
 }
