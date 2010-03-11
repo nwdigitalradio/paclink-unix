bd5ba14b75132c2988b7933bead37c9dea880370
basilgunn <>
2010-03-11 22:46:15 +0000

    This file holds the current version # for this project.

---
+++ paclink-unix/version.h
@@ -0,0 +1,39 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 2; c-brace-offset: -2; c-argdecl-indent: 2 -*- */
+
+/*  paclink-unix client for the Winlink 2000 ham radio email system.
+ *
+ *  Copyright 2006 Nicholas S. Castellano <n2qz@arrl.net> and others,
+ *                 See the file AUTHORS for a list.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+/* $Id$ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define PLU_MAJOR_VERSION   0
+#define PLU_MINOR_VERSION   5
+#define PLU_BUILD           0
+
+#define PLU_VERSION_STRING \
+    __STRING(PLU_MAJOR_VERSION) \
+    "." \
+    __STRING(PLU_MINOR_VERSION)\
+    "("__STRING(PLU_FLASH_BUILD)")"
+
+#endif /* VERSION_H */
