c0deb460282c326fe73fe5c19250bc6db4781033
n7nix <basil@pacabunga.com>
2018-04-21 17:55:10 -0700

    Add build with no install option.

--- buildall.sh
+++ buildall.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #
 # Build & install paclink-unix
+#  - any command line args causes build from clean
 #
 #DEBUG=1
+#MAKE_NO_INSTALL="true"
+MAKE_NO_INSTALL=
 
 scriptname="`basename $0`"
 
 # MTA sets command line option for sendmail
 #   Defaults to sendmail flags -ba
 #   Setting MTA to --enable-postfix sets sendmail flags to -bm
-# If not using postfix as the mta comment next line
+# If not using postfix as the mta then comment next line
 MTA="--enable-postfix"
 
 clean_arg="false"
@@ -91,33 +94,35 @@ echo "=== making paclink-unix using $num_cores cores"
 make -j$num_cores >> build_log.out 2>> build_error.out
 if [ "$?" -ne 0 ] ; then echo "build failed at make"; exit 1; fi
 
-echo "=== installing paclink-unix"
-sudo make install >> build_log.out 2>> build_error.out
-if [ "$?" -ne 0 ] ; then echo "build failed at make install"; exit 1; fi
+if [ "$MAKE_NO_INSTALL" != "true" ] ; then
+   echo "=== installing paclink-unix"
+   sudo make install >> build_log.out 2>> build_error.out
+   if [ "$?" -ne 0 ] ; then echo "build failed at make install"; exit 1; fi
 
-if [ ! -z "$DEBUG" ] ; then
-   echo "=== test for required build files 'missing' & 'test-driver'"
-   ls -salt $CUR_DIR/missing $CUR_DIR/test-driver
-fi
-
-echo "=== verifying paclink-unix install"
-REQUIRED_PRGMS="wl2ktelnet wl2kserial wl2kax25 wl2kax25d"
-
-install_status="OK"
-for prog_name in `echo ${REQUIRED_PRGMS}` ; do
-   type -P $prog_name &>/dev/null
-   if [ $? -ne 0 ] ; then
-      echo "$scriptname: paclink-unix not installed properly"
-      echo "$scriptname: Need to Install $prog_name program"
-      install_status="FAIL"
+   if [ ! -z "$DEBUG" ] ; then
+      echo "=== test for required build files 'missing' & 'test-driver'"
+      ls -salt $CUR_DIR/missing $CUR_DIR/test-driver
    fi
-done
-echo "Check for required installed files ... $install_status"
 
-if [[ $EUID != 0 ]] ; then
-   user=$(whoami)
-   sudo chown -R $user:mail /usr/local/var/wl2k
-fi
+   echo "=== verifying paclink-unix install"
+   REQUIRED_PRGMS="wl2ktelnet wl2kserial wl2kax25 wl2kax25d"
+
+   install_status="OK"
+   for prog_name in `echo ${REQUIRED_PRGMS}` ; do
+      type -P $prog_name &>/dev/null
+      if [ $? -ne 0 ] ; then
+         echo "$scriptname: paclink-unix not installed properly"
+         echo "$scriptname: Need to Install $prog_name program"
+         install_status="FAIL"
+      fi
+   done
+   echo "Check for required installed files ... $install_status"
+
+   if [[ $EUID != 0 ]] ; then
+      user=$(whoami)
+      sudo chown -R $user:mail /usr/local/var/wl2k
+   fi
+fi  # MAKE_NO_INSTALL == true
 
 echo
 echo "$(date "+%Y %m %d %T %Z"): paclink-unix build script FINISHED"
