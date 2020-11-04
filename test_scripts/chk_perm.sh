84b80925b54cea73a9c21befa788545433b9c569
n7nix <basil@pacabunga.com>
2020-11-04 15:31:14 -0800

    Check permissions on message id database file.

--- test_scripts/chk_perm.sh
+++ test_scripts/chk_perm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PLU_VAR_DIR="/usr/local/var/wl2k"
+MID_DB_FILE="$PLU_VAR_DIR/mid.db"
 
 USER=$(whoami)
 GROUP="mail"
@@ -20,7 +21,16 @@ if [ $(stat -c "%U" $PLU_VAR_DIR) != "$USER" ]  || [ $(stat -c "%G" $PLU_VAR_DIR
    sudo chown -R $USER:$GROUP $PLU_VAR_DIR
    echo "New permissions: $(stat -c "%U" $PLU_VAR_DIR):$(stat -c "%G" $PLU_VAR_DIR)"
 else
-   echo "permissions OK: $(stat -c "%U" $PLU_VAR_DIR):$(stat -c "%G" $PLU_VAR_DIR)"
+   echo "permissions OK on directory $PLU_VAR_DIR: $(stat -c "%U" $PLU_VAR_DIR):$(stat -c "%G" $PLU_VAR_DIR)"
+fi
+
+echo "=== test owner & group id of message id database"
+if [ $(stat -c "%U" $MID_DB_FILE) != "$USER" ]  || [ $(stat -c "%G" $MID_DB_FILE) != "$GROUP" ] ; then
+   echo "MID database has wrong permissions: $(stat -c "%U" $MID_DB_FILE):$(stat -c "%G" $MID_DB_FILE)"
+   sudo chown $USER:$GROUP $MID_DB_FILE
+   echo "New permissions: $(stat -c "%U" $MID_DB_FILE):$(stat -c "%G" $MID_DB_FILE)"
+else
+   echo "permissions OK on file $MID_DB_FILE: $(stat -c "%U" $MID_DB_FILE):$(stat -c "%G" $MID_DB_FILE)"
 fi
 
 filecount=$(ls -1 ${PLU_VAR_DIR}/outbox | wc -l)
